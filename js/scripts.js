65958e00936dd2338b62a676959c89a5b1ed181d
Grillo PC <carlsenrmt26@gmail.com>
2023-10-11 14:13:13 -0400

    modified

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+// Función para agrandar la imagen
+// Función para abrir el modal
+function abrirModal() {
+    var fondoModal = document.getElementById("fondo-modal");
+    fondoModal.style.display = "block";
+  }
 
+  // Función para cerrar el modal
+  function cerrarModal() {
+    var fondoModal = document.getElementById("fondo-modal");
+    fondoModal.style.display = "none";
+  }
