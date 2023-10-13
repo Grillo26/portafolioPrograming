c284e41fc885e6e164f0b093b7f20712e2e2055f
Grillo PC <carlsenrmt26@gmail.com>
2023-10-13 17:40:35 -0400

    last proyects add

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,22 @@
 // Use this file to add JavaScript to your project
 // Función para agrandar la imagen
 // Función para abrir el modal
-function abrirModal() {
-    var fondoModal = document.getElementById("fondo-modal");
-    fondoModal.style.display = "block";
-  }
 
-  // Función para cerrar el modal
-  function cerrarModal() {
-    var fondoModal = document.getElementById("fondo-modal");
-    fondoModal.style.display = "none";
-  }
+
+document.getElementById('miFormulario').addEventListener('submit', function(e) {
+    e.preventDefault(); // Evita que el formulario se envíe normalmente
+
+    var nombre = document.getElementById('Nombre').value;
+    var correo = document.getElementById('Correo').value;
+    var numero = document.getElementById('Numero').value;
+    var mensaje = document.getElementById('Mensaje').value;
+
+    var url = "https://script.google.com/macros/s/AKfycby77QY_d6hrNl3cP7CHWRicsQdrIBKTKaS7vpmuvL8PUsnlWp9No9VAiXT7p5TCeewrkA/exec"; // Reemplaza con la URL de la aplicación web de Google Apps Script
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+    xhr.send("nombre=" + nombre + "&correo=" + correo + "numero"+ numero + "&mensaje=" + mensaje);
+
+    // Puedes agregar código adicional para manejar la respuesta del servidor
+});
